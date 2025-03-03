d5695c4e9e6a0d8b198a8fec60d0d993145beed3
Anitaraut1499 <anitaraut357@gmail.com>
2025-03-03 13:08:45 +0530

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,4 @@
+#Write a function greet(name) that takes a name ehich is store in variable as input and prints "Hello, [name]!".
+def greet(name):
+    print(f"Hello, {name}!")
+greet("Alice")
---
+++ Question4.py
@@ -0,0 +1,7 @@
+#Define a function sum_numbers(a, b=10) that takes two numbers and returns their sum. If the second number is not provided, it should default to 10.
+def sum_numbers(a, b=10):
+    return a + b
+
+
+print(sum_numbers(5))      # Output: 15 (5 + 10)
+print(sum_numbers(5, 10))   # Output: 12 (5 + 10)
